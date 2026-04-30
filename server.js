217651c694f7d258ae5a3acab841fbbab49bb054
theniteshdev <0911nitesh@gmail.com>
2026-04-30 20:09:57 +0530

    minial things done

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+import { createReadStream } from 'node:fs';
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
+    if (req.url == '/') {
+        const readStream = createReadStream('./public/index.html');
+        readStream.pipe(res);
+    } else {
+        const readStream = createReadStream(`./public${req.url}`);
+        readStream.pipe(res);
 
-    res.write("Hello World from nodejs server");
-    res.end();
+        readStream.on("error", (err) => {
+            const readStream = createReadStream(`./public/pages/404.html`);
+            readStream.pipe(res);;
+
+            readStream.on("error", (err) => {
+                res.write(err.message)
+            })
+        });
+    }
 });
 
 
